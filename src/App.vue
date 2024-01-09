cfa62421a8e252b650132deea3c0354626f0c3e8
SquallStrifeX <squallstrifex@gmail.com>
2024-01-09 18:30:43 +0100

    end exercise

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,15 @@
-<script setup>
+<script>
 import AppHeader from './components/AppHeader.vue'
 import AppMain from './components/AppMain.vue'
 import AppFooter from './components/AppFooter.vue'
+
+export default {
+    components: {
+        AppHeader,
+        AppMain,
+        AppFooter,
+    },
+    }
 </script>
 
 <template>
